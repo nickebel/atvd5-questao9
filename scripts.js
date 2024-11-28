0ff30d54b69077612301039e5b0f843f0540c4bb
nickebel <nas20@aluno.ifal.edu.br>
2024-11-28 15:01:11 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,12 @@
+
+let numero = parseFloat(prompt('Digite um número: '));
+
+if (numero < 0) {
+    
+    alert("Você digitou um número negativo.");
+} else {
+    
+    alert("Você digitou um número positivo.");
+}
+
+
